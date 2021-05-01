b1e847208a49d11905f6e9f668f0fa06935366d2
Alexander Augusto <alexaasf_10@hotmail.com>
2021-05-01 14:34:27 -0300

    start next js project with ESLint, Prettier and other basic configurations

---
+++ src/pages/_document.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+export default class MyDocument extends Document {
+  render(): JSX.Element {
+    return (
+      <Html lang="pt">
+        <Head>
+          <meta charSet="utf-8" />
+
+          <link
+            href="https://fonts.googleapis.com/css?family=Roboto:400,500,700"
+            rel="stylesheet"
+          />
+
+          <link rel="icon" href="/logo.png" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Head from 'next/head'
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <Head>
+        <title>Rede Social</title>
+      </Head>
+
+      <main>
+        <p>Ok</p>
+      </main>
+    </div>
+  )
+}
+
+export default Home
