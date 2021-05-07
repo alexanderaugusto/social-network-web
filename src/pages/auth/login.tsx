cdaa61e997eeed76fe1c89abf6673006e50ce403
Alexander Augusto <alexaasf_10@hotmail.com>
2021-05-06 22:43:54 -0300

    create login page

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Head from 'next/head'
+import { useRouter } from 'next/router'
+import { useAuth } from '../../contexts/auth'
+import { Button, Input } from '../../components'
+
+import LazyLogo from '../../assets/lazy-black.png'
 
 const Login: React.FC = () => {
-  console.log('loop')
+  const auth = useAuth()
+  const router = useRouter()
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  async function login() {
+    await auth
+      .signIn(email, password)
+      .then(() => {
+        router.push('/')
+      })
+      .catch(err => {
+        console.error(err)
+      })
+  }
+
   return (
     <div>
       <Head>
@@ -10,7 +30,40 @@ const Login: React.FC = () => {
       </Head>
 
       <main className="login-page">
-        <p>Login</p>
+        <div className="login-container">
+          <img src={LazyLogo} alt="Lazy Logo" />
+
+          <form
+            onSubmit={e => {
+              e.preventDefault()
+              login()
+            }}
+          >
+            <Input
+              type="email"
+              placeholder="Ex: alexaasf1010@gmail.com"
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+              label="E-mail"
+            />
+            <Input
+              type="password"
+              placeholder="●●●●●●●●●●●●●"
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+              label="Senha"
+            />
+            <Button id="btn-login" type="submit">
+              Entrar
+            </Button>
+          </form>
+          <Button
+            id="btn-register"
+            onClick={() => router.push('/auth/register')}
+          >
+            Criar conta
+          </Button>
+        </div>
       </main>
     </div>
   )
